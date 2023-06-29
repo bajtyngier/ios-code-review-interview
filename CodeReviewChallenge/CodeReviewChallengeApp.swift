df165f2ad93d77d1de4f58742c5ce5cbfcabc886
Konrad Bajtyngier <konrad@vivino.com>
2023-06-29 11:54:14 +0200

    Initial Commit

---
+++ CodeReviewChallenge/CodeReviewChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeReviewChallengeApp.swift
+//  CodeReviewChallenge
+//
+//  Created by Konrad Bajtyngier on 29/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CodeReviewChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
